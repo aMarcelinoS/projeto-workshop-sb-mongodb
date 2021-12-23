3e32fbdcde3f4dd8b9634abee2fd36a8e2ddd180
Alexandre Marcelino <amsantos9@gmail.com>
2021-12-23 09:23:34 -0300

    Primeiro commit -m - Projeto criado

---
+++ src/main/java/com/example/workshopmongosbjava/WorkshopMongoSbJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshopmongosbjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongoSbJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongoSbJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workshopmongosbjava/WorkshopMongoSbJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshopmongosbjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoSbJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
