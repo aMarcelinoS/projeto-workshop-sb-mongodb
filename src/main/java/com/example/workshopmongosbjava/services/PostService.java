e605bfdbf93a71cb90ea05c4d76002b60f26af11
Alexandre Marcelino <amsantos9@gmail.com>
2021-12-25 08:41:51 -0300

    Obtendo um post por id

---
+++ src/main/java/com/example/workshopmongosbjava/services/PostService.java
@@ -0,0 +1,22 @@
+package com.example.workshopmongosbjava.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.workshopmongosbjava.domain.Post;
+import com.example.workshopmongosbjava.repository.PostRepository;
+import com.example.workshopmongosbjava.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}	
+}
